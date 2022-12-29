69350eb2c9245f55bd31855ef66dab1def9cf3c1
teja <tejapeddiboyina7@gmail.com>
2022-12-28 19:25:21 -0600

    added dots

--- src/pages/DetailsScreen.js
+++ src/pages/DetailsScreen.js
@@ -7,7 +7,7 @@ export default class DetailsScreen extends React.Component {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>Details Screen</Text>
           <Button
-            title="Go to Details... again"
+            title="Go to Details... again...."
             onPress={() => this.props.navigation.push('Details')}
           />
           <Button
